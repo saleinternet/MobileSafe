000001655027e469dfd6371d54d264866f768e1d
saleinternet <ngchensiwen@gmail.com>
2015-08-14 08:09:27 +0800

    360手机卫士设置中心界面以及自定义组合控件和属性

---
+++ app/src/main/java/com/csw/ms/mobilesafe/SettingActivity.java
@@ -0,0 +1,61 @@
+package com.csw.ms.mobilesafe;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.csw.ms.mobilesafe.ui.SettingItemView;
+
+public class SettingActivity extends Activity {
+
+    private static final String TAG = "SettingActivity";
+    /**
+     * 升级更新
+     * */
+    private SettingItemView siv_udpate;
+
+    private SharedPreferences sp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        sp = getSharedPreferences("config", MODE_PRIVATE);
+        siv_udpate = (SettingItemView) findViewById(R.id.siv_update);
+
+        //自动升级是否开启
+        boolean update = sp.getBoolean("config",false);
+        Log.i(TAG, "update : "+update);
+        if (update) {
+            //自动升级已经开启
+            siv_udpate.setChecked(true);
+        } else {
+            //自动升级已经关闭
+            siv_udpate.setChecked(false);
+        }
+
+
+        siv_udpate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = sp.edit();
+                //判断是否选中
+                if (siv_udpate.isChecked()) {
+                    //已经打开自动升级
+                    siv_udpate.setChecked(false);
+                    editor.putBoolean("update", false);
+                } else {
+                    //没有打开自动升级
+                    siv_udpate.setChecked(true);
+                    editor.putBoolean("update", true);
+                }
+
+                editor.commit();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/csw/ms/mobilesafe/ui/SettingItemView.java
@@ -0,0 +1,110 @@
+package com.csw.ms.mobilesafe.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.csw.ms.mobilesafe.R;
+
+/**
+ * 自定义的组合控件,里面有两个TextView,一个CheckBox和View
+ * Created by chensiwen on 15/8/10.
+ */
+public class SettingItemView extends RelativeLayout {
+
+    /**
+     * 标题
+     */
+    private TextView tv_title;
+
+    /**
+     * 描述信息
+     */
+    private TextView tv_desc;
+
+    /**
+     * 状态
+     */
+    private CheckBox cb_status;
+
+    private String desc_on;
+    private String desc_off;
+
+    public SettingItemView(Context context) {
+        super(context);
+        initView(context);
+    }
+
+    /**
+     * 布局文件使用的时候调用
+     * */
+    public SettingItemView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView(context);
+
+        String title =
+                attrs.getAttributeValue("http://schemas.android.com/apk/res-auto", "name");
+        desc_on =
+                attrs.getAttributeValue("http://schemas.android.com/apk/res-auto", "desc_on");
+        desc_off =
+                attrs.getAttributeValue("http://schemas.android.com/apk/res-auto", "desc_off");
+
+        System.out.println("desc : "+desc_on+ "#" + desc_off);
+        setTitle(title);
+    }
+
+    public SettingItemView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView(context);
+    }
+
+    /**
+     * 初始化布局文件
+     * @param context
+     * */
+    private void initView(Context context) {
+        //把一个布局文件-->View 并且加载在SettingItemView中
+        View.inflate(context, R.layout.setting_item_view, this);
+
+        tv_title = (TextView) findViewById(R.id.tv_title);
+        tv_desc = (TextView) findViewById(R.id.tv_desc);
+        cb_status = (CheckBox) findViewById(R.id.cb_status);
+    }
+
+    /**
+     * 校验组合控件是否选中
+     * */
+    public boolean isChecked() {
+        return cb_status.isChecked();
+    }
+
+    /**
+     * 设置组合控件状态
+     * */
+    public void setChecked(boolean checked) {
+        if (checked) {
+            setDesc(desc_on);
+        } else {
+            setDesc(desc_off);
+        }
+        cb_status.setChecked(checked);
+    }
+
+    /**
+     * 设置标题信息
+     * */
+    public void setTitle(String title) {
+        tv_title.setText(title);
+    }
+
+    /**
+     * 设置描述信息
+     * */
+    public void setDesc(String desc) {
+        tv_desc.setText(desc);
+    }
+
+}
